4198fed80bc7d311b7ec171769a2733249c04174
Francisco-Nicolas-Elias <franeliasbv@gmail.com>
2024-04-22 10:53:24 -0300

    Creada clsArbolBinario y procedimiento agregar al árbol binario creado

---
+++ pryEstructuraDatos/clsArbolBinario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEstructuraDatos
+{
+    internal class clsArbolBinario
+    {
+        //Creo el campo inicial del árbol. Lo llamamos raíz
+        private clsNodo PrimerNodo;
+
+        //Creo la única propiedad que necesito
+        public clsNodo Raiz
+        {
+            get { return PrimerNodo; }
+            set { PrimerNodo = value; }
+        }
+
+        public void AgregarArbolBinario(clsNodo Nuevo)
+        {
+            Nuevo.Izquierdo = null;
+            Nuevo.Derecho = null;
+            if (Raiz == null)
+            {
+                Raiz = Nuevo;
+            }
+            else
+            {
+                clsNodo NodoPadre = Raiz; //ant
+                clsNodo Aux = Raiz;
+
+                while (Aux != null)
+                {
+                    NodoPadre = Aux;
+
+                    if (Nuevo.Codigo < Aux.Codigo)
+                    {
+                        Aux = Aux.Izquierdo;
+                    }
+                    else
+                    {
+                        Aux = Aux.Derecho;
+                    }
+                }
+
+                //Fuera del Mientras
+                if (Nuevo.Codigo < NodoPadre.Codigo)
+                {
+                    NodoPadre.Izquierdo = Nuevo;
+                }
+                else
+                {
+                    NodoPadre.Derecho = Nuevo;
+                }
+            }
+        }
+    }
+}
